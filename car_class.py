fde6575498bd63a2a8fd25c7e6e195f72fdd4913
kdaespir <84219248+kdaespir@users.noreply.github.com>
2023-02-13 18:21:28 -0500

    Update car_class.py

--- car_class.py
+++ car_class.py
@@ -1,1 +1,13 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import KFold
+from sklearn.feature_selection import f_classif
+from sklearn.neighbors import KNeighborsClassifier
 
+
+def car_classify(filename):
+    data = pd.read_csv(filename)
+    print(data.describe())
+
+
+car_classify("pred_comp_3_and_4_training_small_W2023_v1.csv")
