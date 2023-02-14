a6b472dd443d2f1bcd15bea3444b1bd4869dd88d
kdaespir <84219248+kdaespir@users.noreply.github.com>
2023-02-14 17:18:00 -0500

    Update car_class.py

--- car_class.py
+++ car_class.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import KFold, train_test_split
-from sklearn.feature_selection import f_classif
+from sklearn.feature_selection import f_classif, SelectKBest
 from sklearn.neighbors import KNeighborsClassifier
 
 def str_to_num(frame):
@@ -14,7 +14,13 @@ def str_to_num(frame):
 
     return legend
 
-def car_classify(filename):
+def feat_sel(xdata, ydata):
+
+    model = SelectKBest(score_func=f_classif, k="all")
+    model.fit(xdata,ydata)
+    return model.scores_
+
+def data_process(filename):
     data = pd.read_csv(filename)
 
     lis_city = data["city"].to_numpy()
@@ -40,11 +46,21 @@ def car_classify(filename):
     labeled_price = [item >= 14500 for item in data["price"].to_numpy()]
     data["labeled_price"] = labeled_price
 
+    return data
+def car_classify(filename):
+    
+    data = data_process(filename)
     xdata = data.drop(["price", "labeled_price"], axis=1)
     ydata = data["labeled_price"]
 
+    features = feat_sel(xdata, ydata)
+    print(f"Higher is better \nYear: {features[0]}\nMileage: {features[1]}\nCity: {features[2]}\nState: {features[3]}\
+    \nMake: {features[4]}\nModel: {features[5]}")
+
     xtrain, xtest, ytrain, ytest = train_test_split(xdata, ydata, random_state=0, test_size=0.3)
-    print(ydata.head())
+
+    
+    print(xdata.head())
 
 
 car_classify("pred_comp_3_and_4_training_small_W2023_v1.csv")
