0337c2e61bdd9f68d888ead0a4789ea9c1cf9182
kdaespir <84219248+kdaespir@users.noreply.github.com>
2023-02-15 12:26:42 -0500

    Added robust min max on mileage to improve accuracy

--- car_class.py
+++ car_class.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import KFold, train_test_split
+from sklearn.model_selection import KFold, train_test_split, cross_val_score
 from sklearn.feature_selection import f_classif, SelectKBest
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import RobustScaler
 
 def str_to_num(frame):
     legend = {}
@@ -46,17 +48,32 @@ def data_process(filename):
     labeled_price = [item >= 14500 for item in data["price"].to_numpy()]
     data["labeled_price"] = labeled_price
 
+    scale_mileage = data[["mileage"]]
+    transformer = RobustScaler().fit_transform(scale_mileage)
+    data["mileage"] = transformer
+    print(data.describe())
     return data
 
-def classifier(xdata, ydata, neighbors):
+def classifier(xdata, ydata, neighbors, outfile):
 
     xtrain, xtest, ytrain, ytest = train_test_split(xdata, ydata, random_state=0, test_size=0.3)
 
-    model = KNeighborsClassifier(n_neighbors=neighbors)
+    model = KNeighborsClassifier(n_neighbors=neighbors, weights="distance")
     model.fit(xtrain, ytrain)
 
+    pred = model.predict(xtest)
+    # scores = model.score(pred, ytest)
+
+    cv_scores = cross_val_score(model, xtest, ytest, cv=5, scoring="accuracy")
+    print(cv_scores)
+    output = f"cv_scores mean for {neighbors} neighbors: {np.mean(cv_scores)}\n"
+    print(output)
+    with open(outfile, "a") as f:
+        f.writelines(output)
+        f.close()
+    # print(scores)
     
-def car_classify(filename):
+def car_classify(filename, neighbors, outfile):
     
     data = data_process(filename)
     xdata = data.drop(["price", "labeled_price"], axis=1)
@@ -66,17 +83,14 @@ def car_classify(filename):
     print(f"Higher is better \nYear: {features[0]}\nMileage: {features[1]}\nCity: {features[2]}\nState: {features[3]}\
     \nMake: {features[4]}\nModel: {features[5]}")
 
+    # all_feat = classifier(xdata, ydata, neighbors)
+
+    sig_xdata = xdata.drop(["city", "state"], axis=1)
+    sig_feat = classifier(sig_xdata, ydata, neighbors, outfile)
+
     
 
 
-    
-    print(xdata.head())
-
-
-car_classify("pred_comp_3_and_4_training_small_W2023_v1.csv")
-
-# test = {"test": 0, "call": 12}
-# test["test"] += 1
-# test["test"] += 9
-# test["two"] = 3
-# print(test)
+# car_classify("pred_comp_3_and_4_training_large_W2023_v1.csv", 16)
+for iteration in range(1,101):
+    car_classify("pred_comp_3_and_4_training_large_W2023_v1.csv", iteration, "car_class_output_dist_wght_robscale.txt")
